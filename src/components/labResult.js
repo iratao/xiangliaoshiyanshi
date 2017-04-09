f609d00d2fa637b29259e6ad865b771301683111
Yurong Tao <iratao@gmail.com>
2017-04-09 17:02:47 +0800

    style the lab result page

--- src/components/labResult.js
+++ src/components/labResult.js
@@ -1,7 +1,63 @@
 import React, { Component, PropTypes } from 'react';
-import { StyleSheet, Text, View, Image, Button, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, Dimensions } from 'react-native';
+
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
+  resultCard: {
+    backgroundColor: '#FDFEFE',
+  },
+  cover: {
+    width: 105.6,
+    height: 105.6,
+    borderRadius: 52.8,
+    marginLeft: 30,
+    marginRight: 14.5,
+  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+
+  },
+  title: {
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  mainTitle: {
+    fontSize: 24,
+    fontFamily: 'Helvetica Neue',
+  },
+  subTitle: {
+    fontSize: 16,
+    fontFamily: 'PingFang SC',
+  },
+  pageContentTitle: {
+    fontFamily: 'PingFang SC',
+    fontSize: 20,
+    fontWeight: '300',
+  },
+  pageContentBody: {
+    fontFamily: 'PingFang SC',
+    fontSize: 16,
+    fontWeight: '100',
+  },
+  lineStyle: {
+    height: 1,
+    backgroundColor: '#FFCD0D',
+    marginTop: 6,
+    marginBottom: 15,
+    width: 200.85,
+  },
+  page: {
+    backgroundColor: '#FDFEFE',
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 25,
+    height: SCREEN_HEIGHT,
+  },
 });
 
 class LabResult extends Component {
@@ -13,11 +69,19 @@ class LabResult extends Component {
 		let { labResult } = this.props;
     let result = labResult && labResult.length > 0 ? labResult[0] : {};
 		return (
-			<View>
-				<ScrollView>
-        <Text>{result.nameEN}</Text>
-        <Text>{result.introduction}</Text>
-				</ScrollView>
+			<View style={styles.resultCard}>
+				<View style={styles.header}>
+          <Image style={styles.cover} source={{ uri: result.cover }} />
+          <View style={styles.title}>
+            <Text style={styles.mainTitle}>{result.nameEN}</Text>
+            <Text style={styles.subTitle}>{result.name}</Text>
+          </View>
+        </View>
+        <View style={styles.page}>
+          <Text style={styles.pageContentTitle}>{'简介'}</Text>
+          <View style={styles.lineStyle} />
+          <Text style={styles.pageContentBody}>{result.introduction}</Text>
+        </View>
 			</View>
 		);
 	}
